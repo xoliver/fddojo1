8672b1340b9c2f1190fd44e1444abee8de8530f7
Shobha Kazinka <shobha@fanduel.com>
2015-11-23 14:13:46 +0000

    An incredibly simple map
    No internal walls.

---
+++ map.py
@@ -0,0 +1,78 @@
+DIRECTIONS = {
+    "north": (0, 1),
+    "west": (-1, 0),
+    "south": (0, -1),
+    "east": (1, 0)
+}
+
+
+class Room(object):
+    doors = []
+
+    def __init__(self):
+        pass
+
+    def make_description(self):
+        description = "You are in a room./n"
+        for direction in doors:
+            description += "There is a door to the ()\n".format(door)
+
+
+class Labyrinth(object):
+    player_location = None
+    monster_location = None
+    layout = None
+    width = None
+    height = None
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        layout = []
+        for _ in range(self.height):
+            # To start out, just do a "maze" with doors between all rooms.
+            row = []
+            for _ in range(self.width):
+                room = Room()
+                room.doors = ["north", "south", "east", "west"]
+                row.append(room)
+            layout.append(row)
+
+        # Remove the doors in the outer walls
+        for room in layout[0]:
+            room.doors.remove("north")
+        for inner_row in layout[1:height-1]:
+            inner_row[0].doors.remove("west")
+            inner_row[-1].doors.remove("east")
+        for room in layout[-1]:
+            room.doors.remove("south")
+
+        self.layout = layout
+
+    def text_map(self):
+        """
+        Return maze table in ASCII
+        For debugging purposes
+        "Borrowed" from http://code.activestate.com/recipes/252127-maze-generator/
+        """
+
+        result = '.' + self.width*'_.'
+        result += '\n'
+
+        for i in range(self.height):
+          result += '|'
+
+          for j in range(self.width):
+            if i==self.height-1 or "south" not in self.layout[i][j].doors:
+              result += '_'
+            else:
+              result += ' '
+            if j==self.width-1 or "east" not in self.layout[i][j].doors:
+              result += '|'
+            else:
+              result += '.'
+
+          result += '\n'
+
+        return result
---
+++ map_demo.py
@@ -0,0 +1,4 @@
+from map import Labyrinth
+
+test_lab = Labyrinth(10,10)
+print test_lab.text_map()
